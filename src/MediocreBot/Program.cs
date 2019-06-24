9ed1ecc18cb1f7001326a4f50ac161a9703f4fd7
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-06-24 19:59:40 +0200

    Add empty dotnet core project

---
+++ src/MediocreBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MediocreBot
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
